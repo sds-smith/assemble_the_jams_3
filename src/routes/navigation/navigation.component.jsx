cc441c452e2f5924595265834b9e9b75c3a3c4a7
Shawn Smith <sds.smith24@gmail.com>
2022-07-05 08:05:56 -0400

    add corresponding styles.jsx files to existing components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,22 +7,6 @@ const Navigation = () => {
             <div>Header</div>
             <Outlet />
         </div>
-        // <div className="App">
-        //   <header className="App-header">
-        //     <div className='img-container'>
-        //       <img src={currentUser ? profilePic : logo} className="App-logo" alt="logo" />
-        //     </div>
-        //     <h1>{currentUser ? displayName : 'Please Log In'}</h1>
-        //     <a
-        //       className="App-link" 
-        //       href={SpotifyAuth} 
-        //     > 
-        //       Sign in with Spotify
-        //     </a>
-        //   </header>
-        // </div>
-
-
     )
 }
 
