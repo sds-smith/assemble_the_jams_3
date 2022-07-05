42da23a8984b40b9eda4a74e6ac1da26e07f06e8
Shawn Smith <sds.smith24@gmail.com>
2022-07-05 08:33:59 -0400

    implement and style navigation/header component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,24 @@
+import { Fragment } from "react"
 import { Outlet } from "react-router-dom"
+import SpotifyLogoWhite from '../../assets/icons/Spotify_Logo_RGB_White.png'
+import { Header, SpotifyAttributor, SpotifyLogo } from "./navigation.styles"
 
 const Navigation = () => {
 
     return (
-        <div>
-            <div>Header</div>
+        <Fragment>
+            <Header>
+                <SpotifyAttributor href='https://www.spotify.com/us/premium/' target='_blank' rel="noreferrer" >
+                  <SpotifyLogo src={SpotifyLogoWhite} id='spotify-logo' alt='Spotify Logo'/>
+                  <p>Works with Spotify Premium</p> 
+                </SpotifyAttributor>        
+                <h1>Assemble<span className="highlight">the</span>Jams</h1>
+            </Header>  
             <Outlet />
-        </div>
+        </Fragment>
+
     )
 }
+
 
 export default Navigation
---
+++ src/routes/navigation/navigation.styles.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+    background-color: black;
+    border: 1px solid black;
+
+    h1 {
+        padding: .77rem 0;
+        text-align: center;
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.88rem;
+        color: #fff;
+
+        .highlight {
+            color: green;
+          }
+      }
+      
+`
+
+export const SpotifyAttributor = styled.div`
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    
+    p {
+        font-size: 10px;
+        color: white;
+        padding-top: 2px;
+        padding-left: 21px;
+      }
+`
+
+export const SpotifyLogo = styled.img`
+    width: 70px;
+    height: auto;
+`
