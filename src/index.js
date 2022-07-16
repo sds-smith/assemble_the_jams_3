7e52a948752b32c56d1ab21ebb620687d151f475
Shawn Smith <sds.smith24@gmail.com>
2022-07-16 15:41:12 -0400

    correct register import error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './service-worker'
+import * as serviceWorker from './serviceWorkerRegistration'
 import './index.css';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
