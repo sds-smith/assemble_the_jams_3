145ce01a6c8c8b8712369ca5bcf8a324969e70e0
Shawn Smith <sds.smith24@gmail.com>
2022-07-17 10:03:41 -0400

    clean up unused vars

--- src/components/home-page-components/home-hero/home-hero.component.jsx
+++ src/components/home-page-components/home-hero/home-hero.component.jsx
@@ -2,8 +2,8 @@
 import PlaylistNameInput from '../playlist-name-input/playlist-name-input.component'
 import SearchBar from '../search-bar/search-bar.component'
 
-import plus from '../../../assets/icons/add_white24.png'
-import play from '../../../assets/icons/play_white24.png'
+// import plus from '../../../assets/icons/add_white24.png'
+// import play from '../../../assets/icons/play_white24.png'
 
 import { useMediaQuery } from '../../../utils/customHooks'
 
--- src/components/home-page-components/playlist-name-input/playlist-name-input.component.jsx
+++ src/components/home-page-components/playlist-name-input/playlist-name-input.component.jsx
@@ -1,8 +1,5 @@
-import { useState, useContext } from "react"
+import { useContext } from "react"
 
-import Button from "../../reusable-components/button/button.component"
-
-import { Spotify } from "../../../utils/spotify"
 import { UserContext } from "../../../contexts/user.context"
 import { PlayerContext } from "../../../contexts/player.context"
 import { TrackContext } from "../../../contexts/track.context"
@@ -10,8 +7,6 @@ import { PlaylistNameContainer, NameInput } from "./playlist-name-input.styles"
 
 const PlaylistNameInput = () => {
 
-    const { accessToken } = useContext(UserContext)
-    const { currentPlayer } = useContext(PlayerContext)
     const { playlistName, setPlaylistName } = useContext(TrackContext)
     
     const clearInput = () => {
