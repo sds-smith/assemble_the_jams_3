3a572ffd4cc4d450a819155aaeba4a2836584e6e
Shawn Smith <sds.smith24@gmail.com>
2022-07-18 07:32:42 -0400

    update log-in message

--- src/data/login-message.js
+++ src/data/login-message.js
@@ -1,6 +1,6 @@
 
 export const LogInMessage = [
- 'Search for tracks. Just type a song, artist, or album in the searchbar, and receive a list of tracks that match your search term as well as Spotify recommendations based on fourteen tunable track attributes.  You can even customize how your recommendations are delivered to you using a custom tuner.',
- 'Select the play button inside any track to hear it in the custom WebPlayer. Inside the WebPlayer, you can like/unlike the song (instantly saves to your Spotify profile) or select a link to listen on Spotify.',
+ 'You can search for tracks by song, artist, or album, and receive search term matches as well as Spotify recommendations based on fourteen different track attributes.',
+ 'Select the play button inside any track to hear a preview in the custom WebPlayer. Inside the WebPlayer, you can like/unlike the song (instantly saves to your Spotify profile) or select a link to listen to the full track on Spotify.',
  'Clicking on your profile picture or username in the app takes you to your Spotify profile page.'
 ]
