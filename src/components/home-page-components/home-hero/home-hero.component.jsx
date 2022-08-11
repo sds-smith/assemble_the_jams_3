b43ccaa102e3a632675aa4cf01bac34b4a6321c9
Shawn Smith <sds.smith24@gmail.com>
2022-08-11 14:56:48 -0400

    step by step verbage

--- src/components/home-page-components/home-hero/home-hero.component.jsx
+++ src/components/home-page-components/home-hero/home-hero.component.jsx
@@ -14,7 +14,7 @@ const HomeHero = () => {
 
     const step1 = '1. Name your New Playlist'
     const step2 = '2. Search for tracks'
-    let step3 = `3. Add tracks to your playlist with the plus button.  Hear a preview of any track with the play button.`
+    let step3 = `3. Add tracks to your playlist with the plus button.  Hear a preview of any track with the play button. Repeat step 2 for more Recommendations.`
     const step4 = '4. Once you\'re happy with your playlist, save it to your Spotify account with the \'Save to Spotify\' button'
     step3 += isMobile ? ' Switch between list views with the links below.' : ''
     
