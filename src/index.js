e00fc0bd2fd00c95cd414fe494a8885b4f33e9e9
Shawn Smith <sds.smith24@gmail.com>
2022-08-15 15:37:45 -0400

    install and implement redux persist

--- src/index.js
+++ src/index.js
@@ -5,26 +5,29 @@ import * as serviceWorker from './serviceWorkerRegistration'
 import './index.css';
 
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { PlayerProvider } from './contexts/player.context';
 import { TrackProvider } from './contexts/track.context';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store} >
-      <BrowserRouter >
-        <UserProvider>
-          <TrackProvider>
-            <PlayerProvider>
-              <App />
-            </PlayerProvider>        
-          </TrackProvider>
-        </UserProvider>  
-      </BrowserRouter>
+      <PersistGate persistor={persistor} >
+        <BrowserRouter >
+          <UserProvider>
+            <TrackProvider>
+              <PlayerProvider>
+                <App />
+              </PlayerProvider>        
+            </TrackProvider>
+          </UserProvider>  
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,18 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from "./root-reducer";
 
-export const store = createStore(rootReducer)
+const persistConfig = {
+    key: 'root',
+    storage,
+
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store)
 
