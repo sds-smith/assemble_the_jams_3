c55824e45ca5b1c6fb78a7e3d00288ba72541f4d
Shawn Smith <sds.smith24@gmail.com>
2022-08-15 09:08:07 -0400

    install redux, react-redux

--- src/contexts/auth.context.jsx
+++ src/contexts/auth.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext({
     authSession : null,
@@ -7,9 +7,53 @@ export const AuthContext = createContext({
     setAccessToken : () => null,
 })
 
+export const AUTH_ACTION_TYPES = {
+    SET_AUTH_SESSION : 'SET_AUTH_SESSION',
+    SET_ACCESS_TOKEN : 'SET_ACCESS_TOKEN'
+}
+
+const authReducer = (state, action) => {
+    const { type, payload } = action
+
+    switch(type) {
+        case AUTH_ACTION_TYPES.SET_AUTH_SESSION :
+            return {
+                ...state,
+                authSession : payload
+            }
+        case AUTH_ACTION_TYPES.SET_ACCESS_TOKEN :
+            return {
+                ...state,
+                accessToken : payload
+            }
+        default : 
+            throw new Error(`Unhandled type ${type} in authReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    authSession: '',
+    accessToken: ''
+}
+
 export const AuthProvider = ({children}) => {
-    const [authSession, setAuthSession] = useState('')
-    const [accessToken, setAccessToken] = useState('')
+    const [ state, dispatch ] = useReducer(authReducer, INITIAL_STATE)
+
+    const { authSession, accessToken } = state
+
+    const setAuthSession = (session) => {
+        dispatch({
+            type: AUTH_ACTION_TYPES.SET_AUTH_SESSION,
+            payload: session
+        })
+    }
+
+    const setAccessToken = (token) => {
+        dispatch({
+            type: AUTH_ACTION_TYPES.SET_ACCESS_TOKEN,
+            payload: token
+        })
+    }
 
     const value = { 
                     authSession,
