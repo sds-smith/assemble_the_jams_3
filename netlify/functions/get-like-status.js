2eab74e49d4dedaf36ed4a4820952718d82d9def
Shawn Smith <sds.smith24@gmail.com>
2022-08-23 08:13:15 -0400

    remove netlify functions from gitignore

---
+++ netlify/functions/get-like-status.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const axios = require('axios').default
+const {getAuthDoc} = require('../../src/utils/firebase.node')
+
+exports.handler = async (event) => {
+    try {
+        const {authSession, trackId} = JSON.parse(event.body)
+        const {accessToken} = await getAuthDoc(authSession) 
+
+        const headers = { Authorization : `Bearer ${accessToken}` }
+        const response = await axios.get(`https://api.spotify.com/v1/me/tracks/contains?ids=${trackId}`,{headers : headers})
+        const status = response.data[0]
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify({status})
+        }
+    } catch(error) {
+        console.log({ error })
+        return {
+            statusCode: 400,
+            body: JSON.stringify({error})
+        }
+    }
+}
