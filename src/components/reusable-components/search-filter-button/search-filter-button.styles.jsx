a2e9de3f0d0fa732ea00511c340d9532f5a40de5
Shawn Smith <sds.smith24@gmail.com>
2022-08-25 12:24:53 -0400

    increase height of search filter button

--- src/components/reusable-components/search-filter-button/search-filter-button.styles.jsx
+++ src/components/reusable-components/search-filter-button/search-filter-button.styles.jsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const FilterButtonContainer = styled.button`
     width: 100%;
-    height: 30px;
+    height: 40px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
     background-color: rgba(255, 255, 255, 0);
     border: 1px solid rgba(255, 255, 255, .2);
