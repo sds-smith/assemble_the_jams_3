3e0bdd80495260c8e4acd6369e1483d29bf4591e
Shawn Smith <sds.smith24@gmail.com>
2022-08-25 12:14:56 -0400

    add styling in TermSelector

--- src/components/reusable-components/search-filter-button/search-filter-button.component.jsx
+++ src/components/reusable-components/search-filter-button/search-filter-button.component.jsx
@@ -1,10 +1,11 @@
 
+import SrchBtn from '../../../assets/icons/search_white24.png'
 
 import { FilterButtonContainer } from './search-filter-button.styles'
 
 const SearchFilterButton = ({onClick, children}) => {
     return (
-        <FilterButtonContainer onClick={onClick} >{children}</FilterButtonContainer>
+        <FilterButtonContainer onClick={onClick} >{children}<img src={SrchBtn} alt='search'/></FilterButtonContainer>
     )
 }
 
--- src/components/reusable-components/search-filter-button/search-filter-button.styles.jsx
+++ src/components/reusable-components/search-filter-button/search-filter-button.styles.jsx
@@ -3,7 +3,14 @@ import styled from "styled-components";
 export const FilterButtonContainer = styled.button`
     width: 100%;
     height: 30px;
+    display: flex;
+    justify-content: space-between;
     background-color: rgba(255, 255, 255, 0);
     border: 1px solid rgba(255, 255, 255, .2);
     color: white;
+
+    img {
+        width: 1rem;
+        justify-self: flex-end;
+    }
 `
