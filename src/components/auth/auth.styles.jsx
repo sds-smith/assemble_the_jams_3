3d804348e462d5a720f1954011337d36e05fccda
Shawn Smith <sds.smith24@gmail.com>
2022-08-25 14:10:32 -0400

    font color white in auth component

--- src/components/auth/auth.styles.jsx
+++ src/components/auth/auth.styles.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const AuthContainer = styled.div`
     background-color: green;
+    color: white;
     height: 100%;
     width: 100%;
     display: flex;
