64585ee1450b008ab6ba4ca6f94a42127a4906e6
Shawn Smith <sds.smith24@gmail.com>
2022-09-10 09:28:48 -0400

    wrap audioPreview in useMemo in audio-element

--- src/components/home-page-components/web-player/audio-element/audio-element.component.tsx
+++ src/components/home-page-components/web-player/audio-element/audio-element.component.tsx
@@ -1,12 +1,15 @@
-import { useEffect, Fragment, AudioHTMLAttributes, FC } from "react"
+import { useEffect, useMemo, Fragment, AudioHTMLAttributes, FC } from "react"
 import { useContext } from "react"
 import { PlayerContext } from "../../../../contexts/player.context"
 
 const AudioElement: FC<AudioHTMLAttributes<HTMLAudioElement>> = () => {
     const {nowPlaying} = useContext(PlayerContext)
 
-    const audioPreview = new Audio();
-    audioPreview.volume = 0.5;
+    const audioPreview = useMemo(() => {
+        const audioPreview = new Audio();
+        audioPreview.volume = 0.5;
+        return audioPreview
+    }, [])
 
     useEffect(() => {
         audioPreview.src = nowPlaying.track.preview as string
