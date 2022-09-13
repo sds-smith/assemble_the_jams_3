e8d941ac2a6770a9d33b237926e99a18fd30b257
Shawn Smith <sds.smith24@gmail.com>
2022-09-13 16:41:49 -0400

    setActiveAudioElement in audio-element useEffect, update audioPreview logic

--- src/routes/audio-element/audio-element.component.tsx
+++ src/routes/audio-element/audio-element.component.tsx
@@ -1,4 +1,4 @@
-import { useEffect, AudioHTMLAttributes, FC } from "react"
+import { useEffect, AudioHTMLAttributes, FC, EffectCallback } from "react"
 import { useContext } from "react"
 import Home from "../home/home.component";
 import { PlayerContext } from "../../contexts/player.context"
@@ -7,12 +7,27 @@ const audioPreview = new Audio();
 audioPreview.volume = 0.5;
 
 const AudioElement: FC<AudioHTMLAttributes<HTMLAudioElement>> = () => {
-    const {nowPlaying} = useContext(PlayerContext)
+    const { setActiveAudioElement, nowPlaying, setActive } = useContext(PlayerContext)
 
     useEffect(() => {
+        setActiveAudioElement()
+    }, [])
+
+    useEffect((): ReturnType<EffectCallback> => {
         audioPreview.src = nowPlaying.track.preview as string
         audioPreview.load()
-        audioPreview.play()
+        if (audioPreview.src.length) {
+            setActive(true)
+            audioPreview.play()
+            audioPreview.onended = () => setActive(false)
+        }
+
+        return (): void => {
+            audioPreview.src = nowPlaying.track.preview as string
+            audioPreview.remove()
+            setActive(false)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [nowPlaying])
 
     return (
