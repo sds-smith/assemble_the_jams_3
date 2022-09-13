ec003cb6786c692578f3943237d1a6452b903196
Shawn Smith <sds.smith24@gmail.com>
2022-09-13 14:41:50 -0400

    add responsive context

--- src/utils/context.utils.ts
+++ src/utils/context.utils.ts
@@ -41,6 +41,27 @@ export type UserContextProps = {
     currentUserExists(): boolean;
 }
 
+
+export type ActiveTabType = {
+    'playlist' : boolean;
+    'search_results' : boolean;
+}
+
+export type ActiveViewType = {
+    'input' : boolean;
+    'results' : boolean;
+}
+
+export type ResponsiveContextProps = {
+    isMobile: boolean;
+    activeTab: ActiveTabType;
+    activeView: ActiveViewType;
+    setDesktop(): void;
+    setMobileHome(): void;
+    setMobilePlaylist(): void;
+    setMobileSearchResults(): void;
+}
+
 export type ProviderProps = {
     children?: ReactNode
 }
