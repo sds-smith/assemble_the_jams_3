ebc5b0b303ccf2de0bc8894c41a286b757df7b06
Shawn Smith <sds.smith24@gmail.com>
2022-09-14 12:56:39 -0400

    install and implement redux deep persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,18 @@
 import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
+import { getPersistConfig } from 'redux-deep-persist'
 import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from "./root-reducer";
 
 export type RootState = ReturnType<typeof rootReducer>
 
-const persistConfig = {
+const persistConfig = getPersistConfig({
     key: 'root',
     storage,
-}
+    blacklist: [ 'auth.clientToken' ],
+    rootReducer
+})
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
