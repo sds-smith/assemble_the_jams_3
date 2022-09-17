39c53f4fc59ddb744e09d7a204cf54ad84d562fa
Shawn Smith <sds.smith24@gmail.com>
2022-09-17 16:08:28 -0400

    add user in root reducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./auth/auth.reducer";
 import { trackReducer } from "./track/track.reducer";
+import { userReducer } from "./user/user.reducer";
 
 export const rootReducer = combineReducers({
     auth : authReducer,
-    track : trackReducer
+    track : trackReducer,
+    user : userReducer
 })
