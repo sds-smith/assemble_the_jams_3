29a7b3a6e625c24f6326c7989f976e755e77511b
Shawn Smith <sds.smith24@gmail.com>
2023-12-02 08:10:16 -0500

    rename save btn onclick handler

--- src/components/home-page-components/playlist/playlist.component.tsx
+++ src/components/home-page-components/playlist/playlist.component.tsx
@@ -9,7 +9,7 @@ import editIcon from '../../../assets/icons/edit_white24.png';
 import { ResponsiveContext } from "../../../contexts/responsive.context";
 import { AuthContext } from "../../../contexts/auth.context";
 import { TrackContext } from "../../../contexts/track.context";
-import { savePlaylist as gqlSavePlaylist } from "../../../utils/graphql";
+import { savePlaylist } from "../../../utils/graphql";
 import { TrackType } from "../../../utils/types/track.types";
 import { PlaylistContainer, TitleContainer,  SaveToSpotifyButton } from './playlist.styles';
 
@@ -20,21 +20,23 @@ const Playlist = () => {
   const { currentUserExists } = useContext(AuthContext);
   const { recommendationsArray, playlistName, setPlaylistName, setRecommendationsArray, setSearchResultsArray } = useContext(TrackContext);
 
-  const savePlaylist = async () => {
+  const displaySaveMessage = (saveMsg: string) => {
+    setSavedMessage(saveMsg)
+    setTimeout(() => setSavedMessage(''), 3000); 
+  }
+  const handleSavePlaylist = async () => {
     if (!currentUserExists) {
-      setSavedMessage("Please sign in with your Spotify Premium account")
-      setTimeout(() => setSavedMessage(''), 3000);      
+      displaySaveMessage("Please sign in with your Spotify Premium account");   
     } else {
       const trackURIs: string[] = recommendationsArray.map((track: TrackType) => track.uri);
       try {
-        const response = await gqlSavePlaylist({playlistName, trackURIs});
+        const response = await savePlaylist({playlistName, trackURIs});
         if (response.message === `Playlist "${playlistName}" has been saved to your Spotify account`) {
           setPlaylistName(response.playlistName);
           setRecommendationsArray(response.recommendationsArray);
           setSearchResultsArray(response.searchResultsArray);
         }
-        setSavedMessage(response.message);
-        setTimeout(() => setSavedMessage(''), 3000);
+        displaySaveMessage(response.message);
       } catch(error) {
         console.log(JSON.stringify(error, null, 2));
       };
@@ -42,11 +44,11 @@ const Playlist = () => {
   };
 
   return (
-    <PlaylistContainer isMobile={isMobile} onKeyPress={(e) => e.key === 'Enter' && savePlaylist()}>
+    <PlaylistContainer isMobile={isMobile} onKeyPress={(e) => e.key === 'Enter' && handleSavePlaylist()}>
       <TitleContainer isMobile={isMobile} >
         { !isMobile && <img src={editIcon} alt='edit playlist name' />}
         <PlaylistNameInput width={isMobile ? '100%' : 'unset'} />
-        <SaveToSpotifyButton onClick={savePlaylist} >SAVE TO SPOTIFY</SaveToSpotifyButton>
+        <SaveToSpotifyButton onClick={handleSavePlaylist} >SAVE TO SPOTIFY</SaveToSpotifyButton>
       </TitleContainer>
       <ActionMessage position='absolute' right='50px' width='10rem' >{savedMessage}</ActionMessage>
       <TrackList 
